ec90235e86fedbdd779e2ab6fe697c18dd2ed4d0
Bogdan <bogdan_kolesov@bk.ru>
2022-05-03 22:55:56 +0600

    fix

--- components/views/Home/Home.js
+++ components/views/Home/Home.js
@@ -1,32 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { AppContainer, Button, DevLabel, NumberInput, PageTitle, Remainder, Row } from '../../atoms';
 import { ActionInputItem } from '../../molecules';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
-
-    const inputPlusRef = useRef()
-    const inputMinusRef = useRef()
-
     const [isLoading, setIsLoading] = useState(false);
     const [edit, setEdit] = useState(false);
     const [money, setMoney] = useState(0);
     const [days, setDays] = useState(0);
+    const [plusInput, setPlusInput] = useState(0);
+    const [minusInput, setMinusInput] = useState(0);
     const [savedDefaultValue, setSavedDefaultValue] = useState({
         money: 0,
         days: 0,
-        dayMoney: 0
+        dayMoney: 0,
+        date: null
     });
-
-    const savedItemArray = []
-
+    const [savedArray, setSavedArray] = useState([]);
 
     const setDefaultValue = async () => {
         const defaultValue = {
             money: money,
             days: days,
-            dayMoney: (Number(money) / Number(days)).toFixed(2)
+            dayMoney: (Number(money) / Number(days)).toFixed(2),
+            date: new Date()
         };
         try {
             await AsyncStorage.setItem("defaultValue", JSON.stringify(defaultValue));
@@ -45,6 +43,7 @@ const Home = () => {
         }
     };
 
+    const savedItemArray = []
 
     const setItemsToArray = async () => {
         const itemArray = savedItemArray;
@@ -59,7 +58,8 @@ const Home = () => {
         try {
             const savedItemsArrayValue = await AsyncStorage.getItem("itemArray");
             const currentItemsArrayValue = JSON.parse(savedItemsArrayValue);
-            await setSavedDefaultValue(currentItemsArrayValue)
+            await setSavedArray(currentItemsArrayValue)
+            console.log(savedArray)
         } catch (error) {
             console.log(error);
         }
@@ -71,16 +71,21 @@ const Home = () => {
         setEdit(!edit)
     }
 
+    const setPostValues = async () => {
+        await setItemsToArray()
+        await getItemsFromArray()
+    }
+
     useEffect(() => {
         getDefaultValue()
     }, []);
 
+
+
+
     return (
         <AppContainer>
             <DevLabel />
-            <Text>
-                {/* {new Date().getHours()} */}
-            </Text>
             <PageTitle title='Остаток средств' />
             <Remainder money={savedDefaultValue.money} days={savedDefaultValue.days} dayMoney={savedDefaultValue.dayMoney} />
             {
@@ -99,12 +104,8 @@ const Home = () => {
                         <Button onPress={() => setEdit(!edit)} >Редактировать</Button>
                     </>
             }
-            <ActionInputItem ref={inputPlusRef} add onPress={() => savedItemArray.push(
-                ...{
-                    item: inputRef.current.value
-                }
-            )} />
-            <ActionInputItem ref={inputMinusRef} />
+            <ActionInputItem onChangeText={setPlusInput} onPress={() => setPostValues()} add />
+            <ActionInputItem onChangeText={setMinusInput} />
         </AppContainer>
     );
 }
